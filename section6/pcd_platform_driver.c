771054b209d8b2a146156b3586834b79912e6319
Mohamed Ahmed <mohamedstarwars@gmail.com>
2023-07-19 14:51:05 +0300

    Filling up init and exit functions

--- section6/pcd_platform_driver.c
+++ section6/pcd_platform_driver.c
@@ -9,10 +9,34 @@
 #include <linux/uaccess.h>
 #include <linux/errno.h>
 #include <linux/platform_device.h>
+#include "platform.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Mohamed Ahmed");
 MODULE_DESCRIPTION("Platform driver");
+
+#define MAX_DEVICES 10
+
+/*Device private data structure*/
+struct pcdev_private_data
+{
+	struct pcdev_platform_data pdata;
+	char * buffer;
+	dev_t dev_num;
+	struct cdev cdev;
+};
+
+/*Driver private data structure*/
+struct pcdrv_private_data
+{
+	int total_devices;
+	dev_t device_number_base;
+	struct device * device_pcd;
+	struct class * class_pcd;
+};
+
+/*Global structure which members will be filled in the init function*/
+struct pcdrv_private_data  pcdrv_data;
 
 loff_t pcd_lseek(struct file * filp, loff_t offset, int whence)
 {
@@ -81,15 +105,40 @@ struct platform_driver pcd_platform_driver =
 
 static int __init pcd_platform_driver_init(void)
 {
+	int ret;
+	/*1. Dynamically allocate a device number of MAX_DEVICES*/
+		ret = alloc_chrdev_region(&pcdrv_data.device_number_base,0,MAX_DEVICES,"pcdevs");
+		if (ret < 0)
+		{
+			pr_err("Alloc chrdev failed.\n");
+			return ret;
+		}
+	/*2. Create device class under /sys/class*/
+	pcdrv_data.class_pcd = class_create(THIS_MODULE,"pcd-class");
+	if (IS_ERR(pcdrv_data.class_pcd))
+	{
+		pr_err("Class creation failed.\n");
+		ret = PTR_ERR(pcdrv_data.class_pcd);
+		unregister_chrdev_region(pcdrv_data.device_number_base,MAX_DEVICES);
+		return ret;
+	}
+
+	/*3. Register platform driver*/
 	platform_driver_register(&pcd_platform_driver);
 
-	pr_info("Platform driver loaded.\n");
+	pr_info("PCD Platform driver loaded.\n");
 	return 0;
 }
 
 void __exit pcd_platform_driver_exit(void)
 {
+	/*Unregister the platform driver*/
 	platform_driver_unregister(&pcd_platform_driver);
+
+	/*Class destroy*/
+	class_destroy(pcdrv_data.class_pcd);
+	/*Unregister device numbers for MAX_DEVICES*/
+	unregister_chrdev_region(pcdrv_data.device_number_base,MAX_DEVICES);
 
 	pr_info("Platform driver unloaded.\n");
 }
